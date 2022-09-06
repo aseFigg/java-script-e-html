80ba80b8b514063eea4e9a53f5da053a359c86f0
asefrigg <111322170+aseFigg@users.noreply.github.com>
2022-09-05 22:02:59 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,79 +1,32 @@
-//function calcularMedia(n1, n2, n3){
-  //  let resultado = (n1 + n2 + n3)/3;
-//    return resultado;
-//}
-
-//function calcularExame(nota){
-//    let resultado = 10 - nota;
-//    return resultado;
-//}
-
-// function calcularDelta(a, b, c){
-//     let delta = b**2 - (4*a*c);
-//     return delta;
-// }
-
-// function calcularX1(a, b, delta){
-//     let x1 = (-1*b + Math.sqrt(delta)) / 2 * a;
-//     return x1;
-// }
-
-// function calcularX2(a, b, delta){
-//     let x2 = (-1*b - Math.sqrt(delta)) / 2 * a;
-//     return x2;
-// }
-
-funcion calcularCPF(cpf){
-    
+function pegarItem(item, valor){
+    if ((item == "arroz") || (valor == 10)) {
+        console.log(item +  " Valor:"+ valor);
+    }
 }
+
 
 $("#btOla").click(
     function(){
+        produto =
+        [
+            {"nome": "arroz", "valor":19},
+            {"nome": "feijao", "valor":25},
+            {"nome": "ovo", "valor":18},
+        ];
 
-        let notas = [9,7,4];
+        console.log(produto[0].nome);
+        console.log(produto[1].valor);
+        console.log(produto[2].valor)
 
-        let nome = "samuel";
-   
-        for (i=0;i<notas.length;i++){
-            console.log(notas[i]);
-            
-        for (i=0;i<nome.length;i++){
-          console.log(nome[i]);
-        }
+        //console.log(aluno.nome);
+        //console.log(aluno2.nome);
+
+        //listaCompras = ["arroz", "arroz", "feijao", "ovo"]
+        //valorProduto = [10,     12   ,     10,     1];
+
+        //for (i = 0; i<=3; i++) {
+        //    pegarItem(listaCompras[i], valorProduto[i]);
+        //}
+
     }
-
-
-//$("#btOla").click(
-    //function(){
-    //     cadastro = 
-    //     {
-    //         'Nome':document.getElementById('inputNome').value,
-    //         'Endereco':document.getElementById('inputEndereco').value,
-    //         'Email':document.getElementById('inputEmail').value
-    //     }
-    // alert('Meu Nome é: ' +cadastro.Nome + ' sou de ' + cadastro.Endereco);
-
-            //calculo =
-              //  {
-               //     'n1':document.getElementById('inputN1').value,
-              //      'n2':document.getElementById('inputN2').value,
-               //     'n3':document.getElementById('inputN3').value
-                //}
-            //let media = (parseInt(calculo.n1) + parseInt(calculo.n2) + parseInt(calculo.n3)) / 3;
-        //let media = calcularMedia(
-               // parseInt(calculo.n1),
-               // parseInt(calculo.n2),
-               // parseInt(calculo.n3)
-           // );
-
-            
-            //if (media >= 7) {
-             //   alert('voce foi aprovado');
-           // } else {
-            //    alert('voce ficou de exame');
-            //    let exame = 10 - media;
-           // let exame = calcularExame(media);
-             //   alert('voce precisa tirar ' + exame + ' no exame.');
-           // }
-}
-)
+);
